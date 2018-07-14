ec53557094380b181ee3b66eb5fcd38c8e873d8e
Akshay RG <akshayrg93@gmail.com>
2018-07-14 21:27:37 +0530

    Create and index of docs done

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,2 +1,33 @@
 class DocumentsController < ApplicationController
+  before_action :retrieve_document, only: %i[show edit update destroy]
+
+  def index
+    @documents = Document.all.order('created_at DESC')
+  end
+
+  def new
+    @document = Document.new
+  end
+
+  def create
+    @document = Document.new(document_params)
+
+    if @document.save
+      redirect_to @document
+    else
+      render 'new'
+    end
+  end
+
+  def show; end
+
+  private
+
+  def document_params
+    params.require(:document).permit(:title, :content)
+  end
+
+  def retrieve_document
+    @document = Document.find_by_id(params[:id])
+  end
 end
